ea31517cb6b1fa055ad61fa3fe21bb7954f20b28
Ujjawal Kumar <madara.esoteric.studious@gmail.com>
2024-07-11 23:24:34 +0530

    Routers and all

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,58 +1,61 @@
-import React from "react";
-import MenuItem from "../menu-item/menu-item.component";
-import './directory.styles.scss'
+
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
 
 class Directory extends React.Component {
-    constructor() {
-        super();
-        
-        this.state = {
-            sections: [{
-                title: 'jackets',
-                imageUrl: 'https://images.pexels.com/photos/7679798/pexels-photo-7679798.jpeg',
-                id: 1
-            },
-        
-            {
-                title: 'denim',
-                imageUrl: 'https://images.pexels.com/photos/1425249/pexels-photo-1425249.jpeg',
-                id: 2
-            },
-        
-            {
-                title: 'footwear',
-                imageUrl: 'https://images.pexels.com/photos/3987400/pexels-photo-3987400.jpeg',
-                id: 3
-            },
-            
-            {
-                title: 'mens',
-                imageUrl: 'https://images.pexels.com/photos/26384260/pexels-photo-26384260/free-photo-of-selfie-of-men-in-t-shirts.jpeg',
-                size: 'large',
-                id: 4
-            },
+  constructor() {
+    super();
 
-            {
-                title: 'womens',
-                imageUrl: 'https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg',
-                size: 'large',
-                id: 5
-            }
-            ] 
+    this.state = {
+      sections: [
+        {
+          title: 'hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1,
+          linkUrl: 'hats'
+        },
+        {
+          title: 'jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2,
+          linkUrl: ''
+        },
+        {
+          title: 'sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 3,
+          linkUrl: ''
+        },
+        {
+          title: 'womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          size: 'large',
+          id: 4,
+          linkUrl: ''
+        },
+        {
+          title: 'mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          size: 'large',
+          id: 5,
+          linkUrl: ''
         }
-    }
+      ]
+    };
+  }
 
-
-    render() {
-        return (<div className='directory-menu'>
-            {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-                ))}
-            
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Directory;
