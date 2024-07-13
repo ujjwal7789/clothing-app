cab3dd4d87e76be5a73c7e3ca6ba01d428d79603
Ujjawal Kumar <madara.esoteric.studious@gmail.com>
2024-07-13 12:51:57 +0530

    Add Header and links in header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -9,6 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
